f1790411690351171b4b3818d275c766a5c33c77
NipunLiyanage69 <nipun.rightmo@gmail.com>
2024-09-13 07:04:45 +0530

    psr-4 autoloading

--- init.php
+++ init.php
@@ -2,6 +2,8 @@
  
 ini_set('error_log', realpath(__DIR__) . '/logs/log-' . date('Y-m-d') . '.log');
 
-require_once realpath(__DIR__) . '/services/MyInterface.php';
-require_once realpath(__DIR__) . '/services/MyClass.php';
-require_once realpath(__DIR__) . '/helpers/MyClass.php';
+require_once __DIR__ . '/vendor/autoload.php';
+
+// require_once realpath(__DIR__) . '/services/MyInterface.php';
+// require_once realpath(__DIR__) . '/services/MyClass.php';
+// require_once realpath(__DIR__) . '/helpers/MyClass.php';
